f660574bf7177c37fc104766d4a6d41df3002f90
Shwet Solanki <me@shwet.net>
2015-01-02 13:02:11 +0530

    sync

---
+++ TestProject/Pods/Test-iOS-SDK/TestFramework.framework/Headers/TestFiles.h
@@ -0,0 +1,15 @@
+//
+//  TestFiles.h
+//  TestFramework
+//
+//  Created by Shwet Solanki on 02/01/15.
+//  Copyright (c) 2015 shwetsolanki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestFiles : NSObject
+
++(instancetype)sharedInstance;
+
+@end
