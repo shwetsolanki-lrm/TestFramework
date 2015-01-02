343e526ae7e35124676b427b9e46d340a3953130
Shwet Solanki <me@shwet.net>
2015-01-02 12:49:58 +0530

    sync

---
+++ TestProject/Pods/Test-iOS-SDK/TestFramework.framework/Headers/TestFramework.h
@@ -0,0 +1,20 @@
+//
+//  TestFramework.h
+//  TestFramework
+//
+//  Created by Shwet Solanki on 02/01/15.
+//  Copyright (c) 2015 shwetsolanki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TestFiles.h"
+
+//! Project version number for TestFramework.
+FOUNDATION_EXPORT double TestFrameworkVersionNumber;
+
+//! Project version string for TestFramework.
+FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
+
+
